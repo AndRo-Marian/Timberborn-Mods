60b5cdc8554f581e61f010ea93f590313539d5d4
Andronachi Marian <48178307+AndRo-Marian@users.noreply.github.com>
2023-07-25 22:12:01 +0300

    Add files via upload

---
+++ HideWelcomeWindow/HideWelcomeWindowPlugin.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using TimberApi.ConsoleSystem;
+using TimberApi.ModSystem;
+using Timberborn.MainMenuScene;
+
+namespace HideWelcomeWindow
+{
+    [HarmonyPatch]
+    public class HideWelcomeWindowPlugin : IModEntrypoint
+    {
+        public void Entry(IMod mod, IConsoleWriter consoleWriter)
+        {
+            var harmony = new Harmony("com.github.andro-marian.hidewelcomewindow");
+            
+            harmony.PatchAll();
+        }
+        
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(WelcomeScreenBox), "Show")]
+        public static bool OnWelcomeScreenShow(WelcomeScreenBox __instance)
+        {
+            __instance.ShowMainMenu();
+            return false;
+        }
+
+    }
+}
