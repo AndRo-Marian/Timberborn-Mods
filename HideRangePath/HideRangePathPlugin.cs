760e32738f54dd559369df4d1068618ffc83542a
Andronachi Marian <48178307+AndRo-Marian@users.noreply.github.com>
2023-08-09 21:06:04 +0300

    Add files via upload

---
+++ HideRangePath/HideRangePathPlugin.cs
@@ -0,0 +1,50 @@
+﻿using HarmonyLib;
+using TimberApi.ConsoleSystem;
+using TimberApi.DependencyContainerSystem;
+using TimberApi.ModSystem;
+using Timberborn.BuildingsNavigation;
+using Timberborn.GameScene;
+using Timberborn.InputSystem;
+
+namespace HideRangePath
+{
+    [HarmonyPatch]
+    public class HideRangePathPlugin : IModEntrypoint
+    {
+        private static InputService _key;
+        
+        public void Entry(IMod mod, IConsoleWriter consoleWriter)
+        {
+            var harmony = new Harmony("com.github.andro-marian.hiderangepath");
+            
+            harmony.PatchAll();
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(NewGameInitializer), "Start")]
+        private static void OnGameStarted(NewGameInitializer __instance)
+        {
+            _key = DependencyContainer.GetInstance<InputService>();
+        }
+        
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(BuildingRangeDrawer), "OnSelect")]
+        public static bool OnBuildingSelect(BuildingRangeDrawer __instance)
+        {
+            if (_key.IsShiftHeld)
+            {
+                __instance.DrawRange();
+                __instance.enabled = true;
+            }
+            return false;
+        }
+        
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(BuildingRangeDrawer), "OnPreviewSelect")]
+        public static bool OnBuildingPreviewSelect(BuildingRangeDrawer __instance)
+        {
+            __instance.enabled = _key.IsShiftHeld;
+            return false;
+        }
+    }
+}
